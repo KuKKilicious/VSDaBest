627f858b106d8bfbe1492a109a85bd182a658f7b
KuKKi <KuKKi@DESKTOP-IJEFEAK.fritz.box>
2017-01-26 22:24:41 +0100

    added primary key into user
    registration, fiddling around with file-loader, but have to wait for
    finished article method

--- vs_js/js/public/js/picture.js
+++ vs_js/js/public/js/picture.js
@@ -1,5 +1,5 @@
-var binary
 
+/*
 function binaryToJpg(binary){
 	var img = document.createElement('img');
 	img.src = 'data:image/jpeg;base64,' + btoa('your-binary-data');
@@ -15,21 +15,37 @@ function jpgToBase64(picture){
 	
 }
 
-
-File.prototype.convertToBase64 = function(callback){
-    var reader = new FileReader();
-    reader.onload = function(e) {
-         callback(e.target.result)
+*/
+   // you can do this once in a page, and this function will appear in all your files 
+    File.prototype.convertToBase64 = function(callback){
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                 callback(e.target.result)
+            };
+            reader.onerror = function(e) {
+                 callback(null);
+            };        
+            reader.readAsDataURL(this);
     };
-    reader.onerror = function(e) {
-         callback(null);
-    };        
-    reader.readAsDataURL(this);
-};
 
-$("#picture").on('change',function(){
-var selectedFile = this.files[0];
-selectedFile.convertToBase64(function(base64){
-   alert(base64);
-}) 
-});
+    $('#blah').on('change',function(){
+      var selectedFile = this.files[0];
+      selectedFile.convertToBase64(function(base64){
+           alert(base64);
+      }) 
+    });
+function readURL(input) {
+	console.log("asdhjk");
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
